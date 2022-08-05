f60715eed3a82c583a8c9670e2fd8a04d4a0b9ee
jeffrey-arias <jeffrey.e.arias@gmail.com>
2022-08-05 19:24:48 +0800

    Updated sample data

--- packages/graphiql/src/components/complexity-level-test-data.js
+++ packages/graphiql/src/components/complexity-level-test-data.js
@@ -3,34 +3,34 @@ const complexityLevelDataJSON = {
     complexity: {
       complexityTree: {
         field_name: 'person',
-        estimated_no_of_docs: 0,
+        estimated_no_of_docs: 20,
         additional_cost: 0,
         estimated_cost: 50,
-        actual_no_of_docs: 0,
+        actual_no_of_docs: 16,
         actual_cost: 46,
         children: [
           {
             field_name: 'friends',
-            estimated_no_of_docs: 0,
+            estimated_no_of_docs: 500,
             additional_cost: 0,
             estimated_cost: 510,
-            actual_no_of_docs: 0,
+            actual_no_of_docs: 63,
             actual_cost: 98,
             children: [
               {
                 field_name: 'name',
-                estimated_no_of_docs: 0,
+                estimated_no_of_docs: 2,
                 additional_cost: 0,
                 estimated_cost: 10,
-                actual_no_of_docs: 0,
+                actual_no_of_docs: 2,
                 actual_cost: 9,
               },
               {
                 field_name: 'age',
-                estimated_no_of_docs: 0,
+                estimated_no_of_docs: 5,
                 additional_cost: 0,
                 estimated_cost: 10,
-                actual_no_of_docs: 0,
+                actual_no_of_docs: 2,
                 actual_cost: 9,
               },
             ],
